468bd28d436020d6fbf57a3dde82780dcf1e43ab
shipherman <aleksandr.tyagni@gmail.com>
2023-07-07 12:52:47 +0300

    Draft DB schema

---
+++ ent/schema/order.go
@@ -0,0 +1,29 @@
+package schema
+
+import (
+	"entgo.io/ent"
+	"entgo.io/ent/schema/edge"
+	"entgo.io/ent/schema/field"
+)
+
+// Order holds the schema definition for the Order entity.
+type Order struct {
+	ent.Schema
+}
+
+// Fields of the Order.
+func (Order) Fields() []ent.Field {
+	return []ent.Field{
+		field.Int("ordernum"),
+		field.String("status"),
+	}
+}
+
+// Edges of the Order.
+func (Order) Edges() []ent.Edge {
+	return []ent.Edge{
+		edge.From("user", User.Type).
+			Ref("orders").
+			Unique(),
+	}
+}
