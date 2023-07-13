a1396ce56d67757fc1c1bde50241f82ad7c38da4
shipherman <aleksandr.tyagni@gmail.com>
2023-07-13 16:53:49 +0300

    Add SelectUser

--- lib/db/select.go
+++ lib/db/select.go
@@ -1,4 +1,31 @@
 package db
 
+import (
+	"context"
+
+	"github.com/shipherman/gophermart/ent/user"
+)
+
 // Get bonuses balance for provided user
 func SelectBalance(user string) {}
+
+// Get user by login
+func SelectUserExistence(u, p string) (bool, error) {
+	var exist = false
+	client := GetClient()
+	req, err := client.User.
+		Query().
+		Where(user.LoginEQ(u)).
+		All(context.Background())
+	if err != nil {
+		return exist, err
+	}
+
+	if req[0].Password != p {
+		return exist, nil
+	} else {
+		exist = true
+	}
+
+	return exist, nil
+}
