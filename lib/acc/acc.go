5e29ebafa318ac5cbfafceb858806782b15ec887
shipherman <aleksandr.tyagni@gmail.com>
2023-07-14 20:47:06 +0300

    Change connection to accural; draft

--- lib/acc/acc.go
+++ lib/acc/acc.go
@@ -1,6 +1,25 @@
 // Interaction with accural service
 package acc
 
+import (
+	"fmt"
+
+	"github.com/go-resty/resty/v2"
+)
+
 // Client requests order
 // Parses results
 // Return it to handler
+func ReqAccural() error {
+	client := resty.New()
+
+	resp, err := client.R().EnableTrace().
+		Get("http://localhost:8080/api/order/134")
+	if err != nil {
+		return err
+	}
+
+	fmt.Println(resp)
+
+	return nil
+}
